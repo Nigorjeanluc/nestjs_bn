141fadac8bfa61ee79c0a9ab628c909a55867d39
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-10-14 21:51:43 +0200

    Pipes in nestJS

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -17,6 +17,7 @@ import {
 import { Request, Response } from 'express'
 import { CreateUserDto } from '../../dtos/CreateUser.dto'
 import { UsersService } from '../../services/users/users.service'
+import { ValidateCreateUserPipe } from '../../pipes/validate-create-user/validate-create-user.pipe'
 
 @Controller('users')
 export class UsersController {
@@ -83,8 +84,8 @@ export class UsersController {
 
   @Post('create')
   @UsePipes(new ValidationPipe())
-  createUser(@Body() userData: CreateUserDto) {
-    console.log(userData);
+  createUser(@Body(ValidateCreateUserPipe) userData: CreateUserDto) {
+    console.log(userData.age.toPrecision())
     return this.userService.createUser(userData)
   }
 
---
+++ src/users/pipes/validate-create-user/validate-create-user.pipe.ts
@@ -0,0 +1,29 @@
+import {
+  ArgumentMetadata,
+  Injectable,
+  PipeTransform,
+  HttpException,
+  HttpStatus
+} from '@nestjs/common';
+
+@Injectable()
+export class ValidateCreateUserPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log('Inside ValidateCreateUserPipe');
+    console.log(value);
+    console.log(metadata);
+
+    const parseAgeToInt = parseInt(value.age.toString())
+    if (isNaN(parseAgeToInt)) {
+      console.log(`${value.age} is not a number!`);
+      throw new HttpException(
+        'Invalid Data Type for property age. Expected Number',
+        HttpStatus.BAD_REQUEST,
+      )
+    } else {
+      console.log(`${parseAgeToInt} is a number. Returning...`);
+      return { ...value, age: parseAgeToInt }
+    }
+    return value;
+  }
+}
